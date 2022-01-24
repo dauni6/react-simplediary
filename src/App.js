85b638c71fb9ab3ab9cb22379950a038d24679e3
dauni6 <45280927+dauni6@users.noreply.github.com>
2022-01-24 21:24:16 +0900

    3. React 배열로 렌더링하기
    1. map 함수를 이용하여 각각의 요소를 나타낼 수 있다.
    2. 혹여라도 props가 undefined일까봐 defaultProps를 만들어 뒀다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id:1,
+    author: "홍길동",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id:2,
+    author: "고길동",
+    content: "하이 2",
+    emotion: 3,
+    created_date: new Date().getTime()
+  },
+  {
+    id:3,
+    author: "기무무",
+    content: "하이 3",
+    emotion: 2,
+    created_date: new Date().getTime()
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,25 @@
+import DiaryItem from './DiaryItem';
+import './DiaryList.css'
+
+const DiaryList = ({ diaryList }) => {
+
+    console.log(diaryList);
+
+    return (
+        <div className="DiaryList">
+            <h2>일기 리스트</h2>
+            <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {diaryList.map((diary, idx) => (
+                    <DiaryItem key={diary.id} {...diary} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+DiaryList.defaultProps = {
+    diaryList: []
+};
+
+export default DiaryList;
